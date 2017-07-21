83b17354f046b099ac90d898a4b72f9a20de5e48
Kevin Lausen <OpnSrcConstruction@users.noreply.github.com>
2017-07-20 23:41:55 -0400

    fixed the function...
    added cd into the aliased command

--- .FISHrc/functions/Mk.fish
+++ .FISHrc/functions/Mk.fish
@@ -1,4 +1,4 @@
 # Defined in - @ line 0
-function Mk --description 'alias Mk=/media/zim/M_kitchen/'
+function Mk --description 'alias Mk=cd /media/zim/M_kitchen/'
 	/media/zim/M_kitchen/  $argv;
 end
